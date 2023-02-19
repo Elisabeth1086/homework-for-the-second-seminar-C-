932f68f9a541306b0ebcacf8f120b8ece305ad38
Elisabeth <m.e.elisa@yandex.ru>
2023-02-19 17:26:53 +0300

    Решение задачи 10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+Random rand = new Random();
+int randNumber = rand.Next(100,1000);
+
+System.Console.WriteLine(randNumber);
+
+int desiredNumber = (randNumber/10)%10;  // 456/10->45, 45%10->5 или 456%100->56, 56/10->5 
+
+System.Console.WriteLine(desiredNumber);
