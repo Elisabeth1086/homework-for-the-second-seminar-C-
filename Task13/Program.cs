eed4daf0a03a507e0c2d0da1b19d5a315eed6e51
Elisabeth <m.e.elisa@yandex.ru>
2023-03-03 20:18:55 +0300

    Решение задачи 10,13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -7,33 +7,18 @@
 System.Console.Write("Введите число: ");
 int randNumber=Convert.ToInt32(Console.ReadLine());
 
-if(randNumber>99 && randNumber<1000)  // трехзначное
-{
-    int x=randNumber%10;
-    System.Console.WriteLine(x);
-}
-else if(randNumber>999 && randNumber<10000)  //четырехзначное
-{
-    int x=(randNumber/10)%10;
-    System.Console.WriteLine(x);
-}
-else if(randNumber>9999 && randNumber<100000)  //пятизначное
-{
-    int x=(randNumber/100)%10;
-    System.Console.WriteLine(x);
-}
-else if(randNumber>99999 && randNumber<1000000)  //шестизначное
-{
-    int x=(randNumber/1000)%10;
-    System.Console.WriteLine(x);
-}
-else if(randNumber>999999 && randNumber<10000000)  //семизначное
-{
-    int x=(randNumber/10000)%10;
-    System.Console.WriteLine(x);
-}
-else
-{
-    System.Console.WriteLine("Третьей цифры нет");
-}
+int i=randNumber;
 
+while(i !=0)
+{
+    if(randNumber>99)
+    {
+        int a=i%10;
+        i=i/10;
+        System.Console.Write(a);
+    }
+    else
+    {
+        System.Console.WriteLine("Третьей цифры нет");
+    }
+}
